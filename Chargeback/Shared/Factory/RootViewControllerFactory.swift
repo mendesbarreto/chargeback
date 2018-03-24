fae571b4c3caaacb16f06ab412069b82eaa5be6c
Douglas Mendes <mendes-barreto@live.com>
2018-03-24 17:13:07 -0300

    Add a factory for my root view controller

---
+++ Chargeback/Shared/Factory/RootViewControllerFactory.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  Chargeback
+//
+//  Created by douglas.barreto on 3/24/18.
+//  Copyright © 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewControllerFactory {
+    static func make() -> UIViewController {
+        let viewController = BaseViewController()
+        viewController.view.backgroundColor = .magenta
+        return viewController
+    }
+}
