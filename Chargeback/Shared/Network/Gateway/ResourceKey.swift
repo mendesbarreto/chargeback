6aa851d7f8a5f64647eeb0b831e4fb10d3809708
Douglas Mendes <mendes-barreto@live.com>
2018-03-25 17:13:12 -0300

    Map all resource keys to project
    Help find where the application should go or return in the screen flow

---
+++ Chargeback/Shared/Network/Gateway/ResourceKey.swift
@@ -0,0 +1,15 @@
+//
+// Created by douglas.barreto on 3/25/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import Foundation
+
+enum ResourceKey: String {
+    case notice
+    case chargeback
+    case home
+    case blockCard = "block_card"
+    case unblockCard = "unbl/ock_card"
+    case selfRef = "self"
+}
