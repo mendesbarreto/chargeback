bafced6f476b7d1c892917ff9aa6576d9a31f697
Douglas Mendes <mendes-barreto@live.com>
2018-03-25 18:27:41 -0300

    Create bindable view to update view when a vew model arrives

---
+++ ChargebackTests/Shared/View/BindableView.swift
@@ -0,0 +1,14 @@
+//
+// Created by douglas.barreto on 3/25/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import UIKit
+
+class BindableView<T>: UIView, Bindable {
+    typealias ViewModel = T
+
+    func bind(to: ViewModel) {
+        fatalError("Bind method should be implemented ")
+    }
+}
