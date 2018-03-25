0e5e375135d4acf18340315223048b52c33001aa
Douglas Mendes <mendes-barreto@live.com>
2018-03-25 17:02:45 -0300

    Add new struct will take care of all system static variables

---
+++ Chargeback/Shared/ApplicationProperties.swift
@@ -0,0 +1,10 @@
+//
+// Created by douglas.barreto on 3/25/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import Foundation
+
+struct ApplicationProperties {
+    static let baseUrl = "https://nu-mobile-hiring.herokuapp.com/"
+}
