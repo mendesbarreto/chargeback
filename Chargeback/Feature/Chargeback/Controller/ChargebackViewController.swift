bcd4d3be82181337cf8eeb2d068c687fc2204bb0
Douglas Mendes <mendes-barreto@live.com>
2018-03-31 15:02:43 -0300

    Add chargeback view controller

---
+++ Chargeback/Feature/Chargeback/Controller/ChargebackViewController.swift
@@ -0,0 +1,42 @@
+//
+// Created by douglas.barreto on 3/31/18.
+// Copyright (c) 2018 Douglas Mendes Barreto. All rights reserved.
+//
+
+import UIKit
+
+final class ChargebackViewController: BaseViewController {
+    private let chargebackView: ChargebackView
+    private var showChargebackUserCase: ShowChargebackInformationUseCase!
+
+    override init() {
+        chargebackView = ChargebackView()
+        super.init()
+        showChargebackUserCase = ShowChargebackInformationUseCaseFactory.make(presenterOutput: self)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init?(coder aDecoder: NSCoder) not implemented", file: #file, line: #line)
+    }
+
+    override func viewDidLoad () {
+        super.viewDidLoad()
+        setupChargebackView()
+        showChargebackUserCase.show()
+    }
+
+    private func setupChargebackView() {
+        view.addSubview(chargebackView)
+        chargebackView.anchorToFit(in: view)
+    }
+}
+
+extension ChargebackViewController: ChargebackPresenterOutput {
+    func show (chargebackViewModel: ChargeBackViewModel) {
+        chargebackView.bind(to: chargebackViewModel)
+    }
+
+    func showError () {
+        //TODO: SHOW ALERT WITH ERRO
+    }
+}
