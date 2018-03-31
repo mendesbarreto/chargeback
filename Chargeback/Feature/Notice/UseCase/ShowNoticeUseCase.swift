b14aab740740720ecdf1ff5c82f704b0151dd75a
Douglas Mendes <mendes-barreto@live.com>
2018-03-31 00:36:39 -0300

    Split resource router in files protocol, implementation and factory

--- Chargeback/Feature/Notice/UseCase/ShowNoticeUseCase.swift
+++ Chargeback/Feature/Notice/UseCase/ShowNoticeUseCase.swift
@@ -5,41 +5,6 @@
 
 import RxSwift
 
-final class ResourceRouterFactory {
-    static func make() -> ResourceRouter {
-        return ResourceRouter(noticeGateway: EntryPointResourceMoyaGateway())
-    }
-}
-
-final class ResourceRouter {
-
-    static let main = ResourceRouterFactory.make()
-    let noticeGateway: ResourceGateway & EntryPointResourceGateway
-
-    var currentResource: BaseModel
-
-    init(noticeGateway: ResourceGateway & EntryPointResourceGateway) {
-        self.noticeGateway = noticeGateway
-        currentResource = BaseModel(links: [:])
-    }
-
-    func notice() -> Observable<Notice> {
-        return noticeGateway.requestInitialResource().share().flatMap {[unowned self] (model: BaseModel) -> Observable<Notice> in
-            return self.noticeGateway.request(resource: .notice, in: model.links)
-        }.do(onNext: { [weak self] model in
-            self?.currentResource = model
-        })
-    }
-
-    func chargeBack() -> Observable<Chargeback> {
-        return Observable.empty()
-    }
-
-    func has(resource: ResourceKey) -> Bool {
-        return currentResource.links[resource] != nil
-    }
-}
-
 final class ShowNoticeUseCase {
 
     let noticeGateway: ResourceRouter
